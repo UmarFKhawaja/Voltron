9569b93f5faa0d79d5969add3f0f8a59a20a50e3
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2024-06-27 22:10:07 +0100

    chore: adjust ports

--- apps/monitor-app/src/main.ts
+++ apps/monitor-app/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 6280;
+  const port = process.env.PORT || 2180;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
--- apps/server-app/src/main.ts
+++ apps/server-app/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 6180;
+  const port = process.env.PORT || 2280;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
