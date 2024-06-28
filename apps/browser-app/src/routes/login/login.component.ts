2d6fab8fc5b91962dcfce59f80a845b8e0fdaa2e
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2024-06-28 18:08:47 +0100

    feat: improve /app/login component layout in browser-app app

--- apps/browser-app/src/routes/login/login.component.ts
+++ apps/browser-app/src/routes/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatDividerModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
