efec7f3693e6fa83668559d270eb33864722cf02
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2024-06-28 10:50:31 +0100

    chore: clean up code in monitor-app app

--- apps/monitor-app/src/main.ts
+++ apps/monitor-app/src/main.ts
@@ -1,22 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+
   const port = process.env.PORT || 2180;
+
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  Logger.log(`🚀 Application is running on http://localhost:${port}/${globalPrefix}`);
 }
 
-bootstrap();
+bootstrap().then().catch((error: unknown) => {
+  const { message, stack } = error as Error;
+
+  Logger.error(message, stack);
+});
