ee14c73a6765667bbe554248c91b122e3cf3b54c
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2024-06-29 03:34:58 +0100

    feat: implement reactive forms for login and register forms in browser-app app

--- apps/browser-app/src/routes/login/login.component.ts
+++ apps/browser-app/src/routes/login/login.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,9 +17,38 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatDividerModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formGroup: FormGroup = new FormGroup({
+    username: new FormControl('', (control: AbstractControl) => {
+      const errors: Record<string, ValidationErrors | null> = {
+        email: Validators.email(control),
+        username: Validators.pattern(/^[a-zA-Z_][a-zA-Z0-9_.]+$/)(control)
+      };
+
+      const isValid: boolean = errors['email'] == null || errors['username'] == null;
+
+      return isValid
+        ? null
+        : {
+          invalid: 'Email address or user name must be provided'
+        };
+    }),
+    password: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
 }
--- apps/browser-app/src/routes/register/register.component.ts
+++ apps/browser-app/src/routes/register/register.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, MatButton],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  formGroup: FormGroup = new FormGroup({
+    emailAddress: new FormControl('', Validators.email),
+    userName: new FormControl('', Validators.pattern(/^[a-zA-Z_][a-zA-Z0-9_.]+$/)),
+    password: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
 }
