7c003a82e317b1a3af3ddced9dfd6a50253f2453
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2024-07-02 02:14:56 +0100

    chore: remove support for SSR in browser-app app

--- apps/browser-app/src/core.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app/app.config.server';
-import { RootComponent } from './root/root.component';
-
-const bootstrap = () => bootstrapApplication(RootComponent, config);
-
-export default bootstrap;
