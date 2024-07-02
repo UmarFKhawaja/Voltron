1d990f0b4023ff1534ce3610605857c2728e1fd8
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2024-07-02 11:24:09 +0100

    feat: create an authGuard function to prevent unauthorized access to routes in browser-app app

---
+++ apps/browser-app/src/guards/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { ActivatedRoute, CanActivateFn, Route, Router } from '@angular/router';
+import { constants } from '../../app/app.constants';
+import { RouteClient } from '../../clients/route/route.client';
+import { RouteService } from '../../services/route/route.service';
+import { TokenService } from '../../services/token/token.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  const router: Router = inject(Router);
+  const routeClient: RouteClient = new RouteClient(activatedRoute, router);
+
+  const routeService: RouteService = new RouteService(routeClient);
+  const tokenService: TokenService = inject(TokenService);
+
+  const isAuthenticated: boolean = tokenService.isAuthenticated;
+
+  if (!isAuthenticated) {
+    routeService.navigate(['app', 'show-message', constants.MESSAGES.GENERAL.NOT_AUTHENTICATED], {})
+      .then()
+      .catch();
+  }
+
+  return isAuthenticated;
+};
