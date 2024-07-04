07fbc7d1067b4b4e2abe6d34b6c78fbb9a0f5bd1
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2024-07-04 14:39:04 +0100

    feat: switch to using Material Symbols font for icons

--- apps/browser-app/src/root/root.component.ts
+++ apps/browser-app/src/root/root.component.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from '../components/top-bar/top-bar.component';
 
@@ -10,5 +11,13 @@ import { TopBarComponent } from '../components/top-bar/top-bar.component';
   templateUrl: './root.component.html',
   styleUrl: './root.component.css'
 })
-export class RootComponent {
+export class RootComponent implements OnInit {
+  constructor(
+    private matIconReg: MatIconRegistry
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+  }
 }
