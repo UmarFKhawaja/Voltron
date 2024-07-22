0f9140403f34c3fd33e1a427b299eb33db83fa61
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2024-07-22 16:35:25 +0100

    feat: resolve ESLint errors in services-library library

--- libraries/services-library/src/modules/simple/mjml/mjml.templates/index.ts
+++ libraries/services-library/src/modules/simple/mjml/mjml.templates/index.ts
@@ -1,9 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const COMPLETE_EMAIL_ADDRESS_CHANGE: string = require('./COMPLETE_EMAIL_ADDRESS_CHANGE.mjml');
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const CONFIRM_EMAIL_ADDRESS_CHANGE: string = require('./CONFIRM_EMAIL_ADDRESS_CHANGE.mjml');
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const LOGIN_WITH_MAGIC_LOGIN_MAIL: string = require('./LOGIN_WITH_MAGIC_LOGIN_MAIL.mjml');
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const REGISTER_MAIL: string = require('./REGISTER_MAIL.mjml');
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const RESET_PASSWORD_MAIL: string = require('./RESET_PASSWORD_MAIL.mjml');
